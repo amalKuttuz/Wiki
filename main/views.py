d76aa96f13e5c58db36885ac3a0acc1e583e6389
amalKuttuz <63414371+amalKuttuz@users.noreply.github.com>
2022-08-30 14:41:39 +0530

    second commit

--- main/views.py
+++ main/views.py
@@ -3,13 +3,28 @@ import wikipedia as wp
 # Create your views here.
 def home(request):
     result={}
+    mm={}
+    # title={
+    #     'title':'homepage'
+    # }
     if(request.method=='POST'):
         search=request.POST['search']
     try:
         result=wp.summary(search,sentences=100)
+        title=("title",title)
+        
+       
+
     except:
-        return HttpResponse("The search does not match with any topic in wikipedia")
+         mm=wp.search(search, results=10, suggestion=False)
+         
+        #  mm=wp.suggest(search)
+    #     return HttpResponse("The search does not match with any topic in wikipedia")
+    
     return render(request, 'index.html',{"result":result})
-    return render(request,'index.html')
+    return render(request, 'index.html',{"mm":mm})
+    return render(request, 'index.html',{"title":title})
+
+    # return render(request,'index.html',)
 
 
